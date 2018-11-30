5aae5887d348ed1088969d41a8b5ea2abb5e50ec
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2018-11-30 07:16:57 +0100

    Done with featured carousel

---
+++ src/components/Home/Featured.vue
@@ -0,0 +1,46 @@
+<template>
+    <carousel :per-page="1" :mouse-drag="true" :paginationEnabled="false">
+        
+        <slide v-for="(slide, index) in slides" :key="index">
+            <div class="slider_bck" :style="{'background': `url(${slide.bck})`}">
+                <div class="gradient">
+                    <div class="container">
+                        <div class="content">
+                            <div class="item">{{slide.title}}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </slide>
+    </carousel>
+</template>
+
+<script>
+    import {Carousel, Slide} from 'vue-carousel';
+    import SlideOne from '../../assets/images/featured/1.jpg'
+    import SlideTwo from '../../assets/images/featured/2.jpg'
+    import SlideThree from '../../assets/images/featured/3.jpg'
+    import SlideFour from '../../assets/images/featured/4.jpg' 
+
+export default {
+    components: {
+        Slide,
+        Carousel
+    },
+    data(){
+        return {
+            slides: [
+                {title: 'Red dead redemption', bck: SlideOne},
+                {title: 'Watch Dogs', bck: SlideTwo},
+                {title: 'Rise of the tomb raider', bck: SlideThree},
+                {title: 'Spider-man', bck: SlideFour},
+            ]
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
